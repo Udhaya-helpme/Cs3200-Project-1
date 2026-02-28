93ac21e5fff1d9febac9b7651effc2b4a2082f6e
Xiaodi Wang <wang.x27@northeastern.edu>
2026-02-28 00:14:54 -0800

    verified query4

--- query4.sql
+++ query4.sql
@@ -3,12 +3,11 @@ SELECT
   c.company_name,
   c.industry,
   j.title AS job_title,
-  a.current_status,
-  a.deadline
+  a.current_status
 FROM Application a
 JOIN Student s ON a.student_id = s.student_id
 JOIN JobPosting j ON a.job_id = j.job_id
 JOIN Company c ON j.company_id = c.company_id
-WHERE (c.industry IN ('Technology', 'Finance'))
+WHERE c.industry IN ('Technology', 'Finance')
   AND a.current_status NOT IN ('Rejected', 'Withdrawn')
-  AND a.deadline >= DATE('now');
+ORDER BY s.name, c.company_name;
